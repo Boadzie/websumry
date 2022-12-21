82bb7a54878fe5e4ba0a8ffd47151280c4ad30c0
Daniel Boadzie <boadziedaniel43@gmail.com>
2022-12-21 11:29:31 +0000

    Add openapi tags to add more descriptions to the endpoints

--- app.py
+++ app.py
@@ -4,19 +4,31 @@ from utils import summarize_website
 from typing import Optional, List
 from pydantic import BaseModel
 
-api = FastAPI()
+tags_metadata = [
+    {
+        "name": "Home",
+        "description": "Return welcome message",
+    },
+    {
+        "name": "Summary",
+        "description": "Takes an url, number of sentences and an optional bonus words as input and returns a summary",
+    },
+    
+]
+
+api = FastAPI(openapi_tags=tags_metadata, title="Websumry - summarize an website anytime!")
 
 class SumryInput(BaseModel):
     url:str 
     num_sentences:int 
     bonus_words:Optional[List]
 
-@api.get("/")
+@api.get("/", tags=["Home"])
 def home():
     return {"message": "Welcome to websumry"}
 
 
-@api.post("/summary")
+@api.post("/summary", tags=["Summary"])
 def get_summry(input: SumryInput):
     result = summarize_website(url=input.url, num_sentences=input.num_sentences, bonus_words=input.bonus_words)
     return result 
